40cd650fbd524fd968fb5d6f338e405a3d761dc9
Munir Hussin <profoundseven@gmail.com>
2026-08-02 20:29:39 +0800

    feat: update PUNCTUATIONS format and add devDependencies for packages/error and packages/random

--- packages/random/src/utils/Chars.ts
+++ packages/random/src/utils/Chars.ts
@@ -2,7 +2,7 @@ export const NUMBERS = "0123456789";
 export const HEX = "0123456789abcdef";
 export const ALPHA_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const ALPHA_LOWER = "abcdefghijklmnopqrstuvwxyz";
-export const PUNCTUATIONS = `!"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`;
+export const PUNCTUATIONS = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 export const ALPHA = ALPHA_UPPER + ALPHA_LOWER;
 export const ALPHA_NUMERIC = ALPHA + NUMBERS;
 export const BASE64 = ALPHA_NUMERIC + "+/";
