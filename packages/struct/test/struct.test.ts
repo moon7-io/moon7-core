62f8e63a02327036d92aa9800643be768bd8fe80
Munir Hussin <profoundseven@gmail.com>
2026-08-02 20:44:46 +0800

    fix: update string formatting in Struct.stringify tests for consistency

--- packages/struct/test/struct.test.ts
+++ packages/struct/test/struct.test.ts
@@ -35,12 +35,12 @@ describe("struct", () => {
     });
 
     test("Struct.stringify object should sort keys", () => {
-        expect(Struct.stringify({ a: 1, b: 2 })).toBe(`{"a":1,"b":2}`);
-        expect(Struct.stringify({ b: 2, a: 1 })).toBe(`{"a":1,"b":2}`);
+        expect(Struct.stringify({ a: 1, b: 2 })).toBe('{"a":1,"b":2}');
+        expect(Struct.stringify({ b: 2, a: 1 })).toBe('{"a":1,"b":2}');
     });
 
     test("Struct.stringify nested object should sort keys", () => {
-        const ans = `{"a":1,"b":2,"x":{"p":3,"q":4}}`;
+        const ans = '{"a":1,"b":2,"x":{"p":3,"q":4}}';
         expect(Struct.stringify({ a: 1, x: { p: 3, q: 4 }, b: 2 })).toBe(ans);
         expect(Struct.stringify({ b: 2, x: { p: 3, q: 4 }, a: 1 })).toBe(ans);
         expect(Struct.stringify({ b: 2, x: { q: 4, p: 3 }, a: 1 })).toBe(ans);
